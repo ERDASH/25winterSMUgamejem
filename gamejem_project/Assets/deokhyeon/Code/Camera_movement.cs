5042cea64bb34f7081976d746bd4e78622caa059
ERDASH <gte204343@gmail.com>
2025-12-17 19:22:37 +0900

    카메라움직임 개선
    가장 상단의 dongle을 부드럽게 따라감

--- gamejem_project/Assets/deokhyeon/Code/Camera_movement.cs
+++ gamejem_project/Assets/deokhyeon/Code/Camera_movement.cs
@@ -3,9 +3,12 @@ using System.Linq;
 
 public class Camera_movement : MonoBehaviour
 {
+        private Vector3 velocity = Vector3.zero; // SmoothDamp 계산용 참조 변수
+        public float smoothTime = 0.3f; // 목표 도달까지 걸리는 대략적인 시간
         public float smoothSpeed = 0.125f; // Camera movement speed
         public Vector3 offset; // Camera offset
         public Prefab_spawner prefabSpawner; // Reference to the Prefab_spawner script
+        public float heightThreshold;
 
         private void LateUpdate()
         {
@@ -19,14 +22,18 @@ public class Camera_movement : MonoBehaviour
 
             // Find the dongle with the highest y position
             GameObject highestDongle = placedDongles.OrderByDescending(d => d.transform.position.y).FirstOrDefault();
-
-            if (highestDongle != null)
+            if (highestDongle.GetComponent<DongleMerge>().isGrounded == true)
             {
-                // Calculate the desired camera position
-                Vector3 desiredPosition = new Vector3(transform.position.x, highestDongle.transform.position.y + offset.y, transform.position.z);
-                // Smoothly move the camera to the desired position
-                transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+                if (highestDongle != null)
+                    {
+                    // Calculate the desired camera position
+                    Vector3 desiredPosition = new Vector3(transform.position.x, highestDongle.transform.position.y + offset.y, transform.position.z);
+                    // Smoothly move the camera to the desired position
+                    //transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+                    transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
+                    }
             }
         }
+
 }
 
